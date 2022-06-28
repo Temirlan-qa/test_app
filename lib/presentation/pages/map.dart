619e4e0f3fb277f3479c08d3b95e1eae3d52bcf3
Temirlan-qa <79401245+Temirlan-qa@users.noreply.github.com>
2022-06-28 13:46:31 +0600

    master

--- lib/presentation/pages/map.dart
+++ lib/presentation/pages/map.dart
@@ -1,19 +1,79 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:yandex_mapkit/yandex_mapkit.dart';
 
-class MapPage extends StatelessWidget {
+class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
 
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  Completer<YandexMapController> _completer = Completer();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const <Widget>[
+        children: <Widget>[
           Expanded(
-            child: YandexMap(),
+            child: YandexMap(
+              onMapCreated: _onMapCreated,
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            heroTag: "btn1",
+            onPressed: _zoomIn,
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          FloatingActionButton(
+            heroTag: "btn2",
+            onPressed: _zoomOut,
+            child: const Icon(Icons.remove),
           ),
         ],
       ),
     );
+  }
+
+  void _onMapCreated(YandexMapController controller) {
+    _completer.complete(controller);
+    
+    // controller.getPoint(
+    //   ScreenPoint(x: 43.238949, y: 76.889709)
+    // );
+    controller.moveCamera(
+      CameraUpdate.newBounds(
+        const BoundingBox(
+          northEast: Point(
+            latitude: 43.2389,
+            longitude: 76.8897,
+          ),
+           southWest: Point(
+            latitude: 43.2389,
+            longitude: 76.8897,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _zoomIn() async {
+    YandexMapController controller = await _completer.future;
+    controller.moveCamera(CameraUpdate.zoomIn());
+  }
+
+  Future<void> _zoomOut() async {
+    YandexMapController controller = await _completer.future;
+    controller.moveCamera(CameraUpdate.zoomOut());
   }
 }
